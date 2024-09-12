2d8387cad88fc865fa5d4ee6e5db2239bd9fc222
Pushpendra1001 <pushpendra9122@gmail.com>
2024-09-12 06:26:09 +0530

    update

--- lib/CityScreens/cityMapScreen.dart
+++ lib/CityScreens/cityMapScreen.dart
@@ -1,11 +1,12 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+import 'dart:math';
 import 'package:waterdropdash/CityScreens/dashlandreward.dart';
 import 'package:waterdropdash/GameMenuScreens/ProgressScreen.dart';
-import 'package:waterdropdash/MainGameScreens/MainGame.dart';
-import 'package:waterdropdash/MainGameScreens/RewardScreen.dart';
 import 'package:waterdropdash/provider/GameState.dart';
-
 
 class CityMapScreen extends StatefulWidget {
   const CityMapScreen({Key? key}) : super(key: key);
@@ -15,12 +16,98 @@ class CityMapScreen extends StatefulWidget {
 }
 
 class _CityMapScreenState extends State<CityMapScreen> {
-  List<Map<String, dynamic>> restoreOptions = [
-    {'name': 'Mountain', 'cost': 10, 'progress': 0, 'total': 30, 'position': Offset(150, 150)},
-    {'name': 'Forest', 'cost': 10, 'progress': 0, 'total': 60, 'position': Offset(200, 300)},
-    {'name': 'City', 'cost': 10, 'progress': 0, 'total': 20, 'position': Offset(100, 500)},
-    {'name': 'Badulands', 'cost': 10, 'progress': 0, 'total': 120, 'position': Offset(250, 700)},
-  ];
+  List<Map<String, dynamic>> restoreOptions = [];
+  late Timer _timer;
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadRestoreOptions();
+    _startTimer();
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  void _startTimer() {
+    _timer = Timer.periodic(Duration(hours: 12), (timer) {
+      _checkAndRestoreOptions();
+    });
+  }
+
+  Future<void> _loadRestoreOptions() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? optionsJson = prefs.getString('restoreOptions');
+    if (optionsJson != null) {
+      List<dynamic> decodedOptions = json.decode(optionsJson);
+      setState(() {
+        restoreOptions = decodedOptions.map((option) {
+          return {
+            ...Map<String, dynamic>.from(option),
+            'position': Offset(option['position']['dx'], option['position']['dy']),
+          };
+        }).toList();
+        _isLoading = false;
+      });
+    } else {
+      _initializeRestoreOptions();
+    }
+  }
+
+  void _initializeRestoreOptions() {
+    restoreOptions = [
+      {'name': 'Mountain', 'cost': 10, 'progress': 0, 'total': 30, 'position': Offset(150, 150), 'visible': true, 'lastHiddenTime': null},
+      {'name': 'Forest', 'cost': 10, 'progress': 0, 'total': 60, 'position': Offset(200, 300), 'visible': true, 'lastHiddenTime': null},
+      {'name': 'City', 'cost': 10, 'progress': 0, 'total': 20, 'position': Offset(100, 500), 'visible': true, 'lastHiddenTime': null},
+      {'name': 'Badulands', 'cost': 10, 'progress': 0, 'total': 120, 'position': Offset(250, 700), 'visible': true, 'lastHiddenTime': null},
+    ];
+    _saveRestoreOptions();
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  Future<void> _saveRestoreOptions() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<Map<String, dynamic>> optionsToSave = restoreOptions.map((option) {
+      return {
+        ...option,
+        'position': {'dx': option['position'].dx, 'dy': option['position'].dy},
+      };
+    }).toList();
+    String optionsJson = json.encode(optionsToSave);
+    await prefs.setString('restoreOptions', optionsJson);
+  }
+
+  void _checkAndRestoreOptions() {
+    bool updated = false;
+    for (var option in restoreOptions) {
+      if (!option['visible'] && option['lastHiddenTime'] != null) {
+        DateTime lastHidden = DateTime.parse(option['lastHiddenTime']);
+        if (DateTime.now().difference(lastHidden).inHours >= 12) {
+          option['visible'] = true;
+          option['progress'] = 0;
+          option['position'] = _getRandomPosition();
+          updated = true;
+        }
+      }
+    }
+    if (updated) {
+      setState(() {});
+      _saveRestoreOptions();
+    }
+  }
+
+  Offset _getRandomPosition() {
+    final random = Random();
+    double x = random.nextDouble() * (MediaQuery.of(context).size.width - 100);
+    double y = random.nextDouble() * (MediaQuery.of(context).size.height - 200) + 100;
+    return Offset(x, y);
+  }
 
   void _showRestoreDialog(BuildContext context, Map<String, dynamic> option) {
     final gameState = Provider.of<GameState>(context, listen: false);
@@ -28,9 +115,7 @@ class _CityMapScreenState extends State<CityMapScreen> {
 
     if (currentScore < option['cost']) {
       _showInsufficientFundsDialog(context);
-    } else if(option['progress'] == option['total']){
-      Navigator.push(context, MaterialPageRoute(builder: (context) => Dashlandreward(rewardAmount: 20,badgeName: '',),));
-    } else{
+    } else {
       showDialog(
         context: context,
         builder: (BuildContext context) {
@@ -46,12 +131,20 @@ class _CityMapScreenState extends State<CityMapScreen> {
               ),
               TextButton(
                 onPressed: () {
-gameState.setScore((currentScore - option['cost']).toInt());
+                  gameState.decreaseMainGameScore(option['cost']);
                   setState(() {
                     option['progress'] = (option['progress'] + option['cost']).clamp(0, option['total']);
-                    gameState.decreaseMainGameScore(option['progress']);
+                    if (option['progress'] == option['total']) {
+                      option['visible'] = false;
+                      option['lastHiddenTime'] = DateTime.now().toIso8601String();
+                      _saveRestoreOptions();
+                      Navigator.of(context).pop();
+                      _showRewardScreen(context, option);
+                    } else {
+                      _saveRestoreOptions();
+                      Navigator.of(context).pop();
+                    }
                   });
-                  Navigator.of(context).pop();
                 },
                 child: Text('Restore'),
               ),
@@ -61,6 +154,42 @@ gameState.setScore((currentScore - option['cost']).toInt());
       );
     }
   }
+
+  void _showRewardScreen(BuildContext context, Map<String, dynamic> option) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => Dashlandreward(
+          rewardAmount: Random().nextInt(100),
+          badgeName: '${option['name']} Restorer',
+        ),
+      ),
+    ).then((_) {
+      // After returning from the reward screen, check if we need to reset the option
+      if (option['progress'] == option['total']) {
+        _startCooldown(option);
+      }
+    });
+  }
+
+ void _startCooldown(Map<String, dynamic> option) {
+    setState(() {
+      option['visible'] = false;
+      option['lastHiddenTime'] = DateTime.now().toIso8601String();
+    });
+    _saveRestoreOptions();
+
+    // Schedule the option to reappear after 12 hours
+    Timer(Duration(hours: 12), () {
+      setState(() {
+        option['progress'] = 0;
+        option['visible'] = true;
+        option['position'] = _getRandomPosition();
+      });
+      _saveRestoreOptions();
+    });
+  }
+
 
   void _showInsufficientFundsDialog(BuildContext context) {
     showDialog(
@@ -82,129 +211,109 @@ gameState.setScore((currentScore - option['cost']).toInt());
     );
   }
 
-  void _showAlreadyFill(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Already Restored'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('OK'),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     final gameState = Provider.of<GameState>(context);
 
     return Scaffold(
-      body: Stack(
-        children: [
-          // Background image
-          Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                image: AssetImage("assets/images/dashland.png"),
-              ),
-            ),
-          ),
-          // New app bar
-          Positioned(
-            top: 50,
-            left: 20,
-            right: 20,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: _isLoading
+          ? Center(child: CircularProgressIndicator())
+          : Stack(
               children: [
                 Container(
-                  width: MediaQuery.of(context).size.width / 3,
-                  height: 50,
                   decoration: BoxDecoration(
-                    color: Colors.brown,
-                    borderRadius: BorderRadius.circular(20)
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Image.asset("assets/images/water.png"),
-                      Text("Score", style: TextStyle(color: Colors.white)),
-                      Text('${gameState.mainGameScore}', style: TextStyle(color: Colors.white))
-                    ],
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage("assets/images/dashland.png"),
+                    ),
                   ),
                 ),
-                InkWell(
-                  onTap: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => ProgressScreen()),
-                  ),
-                  child: Image.asset("assets/TipS3.png", height: 50),
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width / 3,
-                  height: 50,
-                  decoration: BoxDecoration(
-                    color: Colors.brown,
-                    borderRadius: BorderRadius.circular(20)
-                  ),
+                Positioned(
+                  top: 50,
+                  left: 20,
+                  right: 20,
                   child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      InkWell(
-                        onTap: () {
-                          Navigator.push(context, MaterialPageRoute(builder: (context) => ProgressScreen()));
-                        },
-                        child: Image.asset("assets/images/life.png")
+                      Container(
+                        width: MediaQuery.of(context).size.width / 3,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          color: Colors.brown,
+                          borderRadius: BorderRadius.circular(20)
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Image.asset("assets/images/water.png"),
+                            Text("Score", style: TextStyle(color: Colors.white)),
+                            Text('${gameState.mainGameScore}', style: TextStyle(color: Colors.white))
+                          ],
+                        ),
                       ),
-                      Text("Lives", style: TextStyle(color: Colors.white)),
-                      Text('${gameState.lives}', style: TextStyle(color: Colors.white))
+                      InkWell(
+                        onTap: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => ProgressScreen()),
+                        ),
+                        child: Image.asset("assets/TipS3.png", height: 50),
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width / 3,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          color: Colors.brown,
+                          borderRadius: BorderRadius.circular(20)
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            InkWell(
+                              onTap: () {
+                                Navigator.push(context, MaterialPageRoute(builder: (context) => ProgressScreen()));
+                              },
+                              child: Image.asset("assets/images/life.png")
+                            ),
+                            Text("Lives", style: TextStyle(color: Colors.white)),
+                            Text('${gameState.lives}', style: TextStyle(color: Colors.white))
+                          ],
+                        ),
+                      ),
                     ],
                   ),
                 ),
+                ...restoreOptions.where((option) => option['visible']).map((option) {
+                  return Positioned(
+                    left: option['position'].dx,
+                    top: option['position'].dy,
+                    child: GestureDetector(
+                      onTap: () => _showRestoreDialog(context, option),
+                      child: Column(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Text('${option['progress']}/${option['total']}'),
+                          ),
+                          SizedBox(height: 5),
+                          Container(
+                            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 8),
+                            decoration: BoxDecoration(
+                              color: Colors.red,
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: Text('Restore', style: TextStyle(color: Colors.white)),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                }).toList(),
               ],
             ),
-          ),
-          // Restore options
-          ...restoreOptions.map((option) {
-            return Positioned(
-              left: option['position'].dx,
-              top: option['position'].dy,
-              child: GestureDetector(
-                onTap: () => _showRestoreDialog(context, option),
-                child: Column(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Text('${option['progress']}/${option['total']}'),
-                    ),
-                    SizedBox(height: 5),
-                    Container(
-                      padding: EdgeInsets.symmetric(horizontal: 15, vertical: 8),
-                      decoration: BoxDecoration(
-                        color: Colors.red,
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Text('Restore', style: TextStyle(color: Colors.white)),
-                    ),
-                  ],
-                ),
-              ),
-            );
-          }).toList(),
-        ],
-      ),
     );
   }
 }
